d5f9a1197f1d872740cd0e27458fa8ac0e16ca3f
Dzmitry Karachun <dzmitry.karachun@bk.ru>
2018-09-16 12:43:37 +0300

    Quadratic equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var array = equation.split(' * ');
+  var part1 = array[1].split(' ');
+  var b = +(part1[1] + part1[2]);
+  var a = +array[0];
+  var part2 = array[2].split(' ');
+  var c = +(part2[1] + part2[2]);
+  var discriminant = b * b - 4 * a * c;
+  discriminant = Math.round(Math.sqrt(discriminant));
+  var x = (-b - discriminant) / (2 * a);
+  var y = (-b + discriminant) / (2 * a);
+  if (x < y) {
+    return [x, y];
+  } else {
+    return [y, x];
+}
 }
